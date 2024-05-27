21743d7c36a09880887bc7218a1e0f65dfc7916d
Vitaliy Vakhnin <ust86@yandex.ru>
2024-05-28 01:05:33 +0500

    new on lab9 second lab 9

--- lab9.py
+++ lab9.py
@@ -1,36 +1,44 @@
 #Лабораторная_9. Вариант_3:Подсчитать количество мужчин и количество женщин, указав спасен/погиб и число или %.
+
 import streamlit as st
 import matplotlib.pyplot as plt
-st.image('titanik.jpg')
+
 info = {"мужчин": 0, "женщин": 0}
 total_passengers = 0
+st.image('titanik.jpg')
 st.header('Информация по пассажирам Титаника, c разделением по половому признаку')
 st.write('Для просмотра информации о пассажирах мужчинах и женщинах выберите соответствующий пункт из списка.')
 selected_value = st.selectbox('Значение поля выжившие:', ['Всего', 'Выживших (1)', 'Погибших (0)'])
 selected_percent = st.checkbox('процент от общего количества пассажиров')
 
-with open("data.csv") as file:
-    for line in file:
-        data = line.split(',')
-        sex = data[5]
-        sur = data[1]
-        if selected_value == "Всего":
-            if sex == 'Age':
-                continue
-            if sex == 'female':
-                info['женщин'] += 1
-            elif sex == 'male':
-                info['мужчин'] += 1
-        elif selected_value == "Выживших (1)" and sur == '1':
-            if sex == 'female':
-                info['женщин'] += 1
-            elif sex == 'male':
-                info['мужчин'] += 1
-        elif selected_value == "Погибших (0)" and sur == '0':
-            if sex == 'female':
-                info['женщин'] += 1
-            elif sex == 'male':
-                info['мужчин'] += 1
+
+def passanger(selected_value):
+    with open('data.csv') as file:
+        for line in file:
+            data = line.split(',')
+            sex = data[5]
+            sur = data[1]
+            if selected_value == "Всего":
+                if sex == 'Age':
+                    continue
+                if sex == 'female':
+                    info['женщин'] += 1
+                elif sex == 'male':
+                    info['мужчин'] += 1
+            elif selected_value == "Выживших (1)" and sur == '1':
+                if sex == 'female':
+                    info['женщин'] += 1
+                elif sex == 'male':
+                    info['мужчин'] += 1
+            elif selected_value == "Погибших (0)" and sur == '0':
+                if sex == 'female':
+                    info['женщин'] += 1
+                elif sex == 'male':
+                    info['мужчин'] += 1
+    return info
+
+
+info = passanger(selected_value)
 
 fig = plt.figure(figsize=(7, 4))
 if selected_percent:
